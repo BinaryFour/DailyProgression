da9488417b9af083dbc553a0b64dc0fdea063c62
binaryFour <kevin.sung.123@gmail.com>
2025-05-15 14:43:58 -0400

    Add functionality to task card

--- src/app/components/taskCard.tsx
+++ src/app/components/taskCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Box from '@mui/material/Box/Box';
@@ -6,34 +6,98 @@ import Checkbox from '@mui/material/Checkbox/Checkbox';
 import CardActions from '@mui/material/CardActions/CardActions';
 import Typography from '@mui/material/Typography/Typography';
 import FormGroup from '@mui/material/FormGroup/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import { Label } from 'app/page';
+import { styled } from '@mui/material/styles';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Delete from '@mui/icons-material/Delete';
+import {
+  CardHeader,
+  colors,
+  FormHelperText,
+  IconButton,
+  Input,
+  InputLabel,
+} from '@mui/material';
+import MoreVert from '@mui/icons-material/MoreVert';
 
 interface TaskCardProps {
-  labels: string[];
+  // labels: Label;
+  title: string;
 }
 
-const TaskCard = (props: TaskCardProps) => {
-  const { labels } = props;
+type TaskItem = {
+  id: string;
+  name: string;
+  isChecked: boolean;
+};
 
-  const taskList = labels.map((label, i) => (
-    <FormControlLabel
-      //index as key codesmell but whatever.  Change if data becomes more important
-      key={i}
-      control={<Checkbox />}
-      label={label}
-      labelPlacement='start'
-    />
-  ));
+const CustomFormControl = styled(FormControl)({
+  // your custom styles go here
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+}) as typeof FormControl;
+
+const emptyTask: TaskItem = {
+  id: crypto.randomUUID(),
+  name: '',
+  isChecked: false,
+};
+
+const TaskCard = (props: TaskCardProps) => {
+  const { title } = props;
+
+  const [taskItems, setTaskItems] = useState<TaskItem[]>([emptyTask]);
+  const addLine = () => {
+    setTaskItems([
+      ...taskItems,
+      {
+        id: crypto.randomUUID(),
+        name: '',
+        isChecked: false,
+      },
+    ]);
+  };
+
+  const removeLine = (id: string) => {
+    setTaskItems(taskItems.filter((taskItem) => taskItem.id !== id));
+  };
 
   const content = (
     <>
-      <CardContent>
-        <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
-          Water House Plants
-        </Typography>
-      </CardContent>
+      <CardHeader
+        sx={{ alignContent: 'center' }}
+        title={<TextField label='Title' />}
+        action={
+          <IconButton aria-label='settings'>
+            <MoreVert />
+          </IconButton>
+        }
+      />
+      <CardContent></CardContent>
       <CardActions>
-        <FormGroup aria-label='position'>{taskList}</FormGroup>
+        <FormGroup aria-label='position'>
+          {taskItems.map((taskItem) => (
+            <CustomFormControl key={taskItem.id}>
+              <IconButton
+                onClick={() => removeLine(taskItem.id)}
+                aria-label='delete'
+              >
+                <Delete />
+              </IconButton>
+              <TextField
+                variant='standard'
+                aria-describedby='my-helper-text'
+                placeholder='Item'
+              />
+              <Checkbox />
+            </CustomFormControl>
+          ))}
+          <Button variant='text' onClick={() => addLine()}>
+            Add Line
+          </Button>
+        </FormGroup>
       </CardActions>
     </>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,55 @@
 'use client';
 
+import Button from '@mui/material/Button';
 import TaskCard from './components/taskCard';
+//base next.js styling
 import styles from './page.module.css';
+import { useState } from 'react';
+
+export type Label = {
+  title: string;
+  checkList: string[];
+};
 
 export default function Home() {
-  const labels = ['Shado Benni', 'Weekly Plants'];
+  const [taskCards, setTaskCards] = useState<Label[]>([]);
+  const exampleLabel: Label = {
+    title: 'Weekly Plants',
+    checkList: ['Shado Benni'],
+  };
+
+  const addTask = () => {
+    //create a TaskCard component and place it in the screen
+    setTaskCards([...taskCards, exampleLabel]);
+  };
+
+  // const TaskContainer = () => {
+  //   return(
+  //     <>
+  //       {taskCards.map((t,i) =>
+  //         <TaskCard labels={t.} key={i} />
+  //       )}
+  //     </>
+  //   )
+  // }
 
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <TaskCard labels={labels} />
+        {/* <TaskCard labels={labels} /> */}
+        <div className={styles.buttonContainer}>
+          <Button
+            className={styles.button}
+            variant='contained'
+            onClick={() => addTask()}
+          >
+            Add Task
+          </Button>
+        </div>
+        <div>
+          <TaskCard title={'New Title'} />
+        </div>
+        {/* <div className={styles.taskGrid}>{taskCards}</div> */}
       </main>
     </div>
   );
